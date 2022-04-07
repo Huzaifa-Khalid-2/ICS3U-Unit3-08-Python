e94626227999ef509711c38d3d2c216cca1cdd58
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-07 08:18:50 -0400

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,26 +2,36 @@
 
 # Created by: Mr. Coxall
 # Created on: Sep 2020
-# This program shows formatting output
+# this function checks if the year inserted by user
+# is a leap year
 
 
 def main():
-    # this function shows formatting output
+    # this function checks if the year inserted by user
+    # is a leap year
 
     # input
-    year = int(input("Enter a year (Positive): "))
+    user_input_string = input("Enter the number of the year (integer): ")
+    print("")
 
-    # process & output
-    if year %4 == 0:
-        if year %100 == 0:
-            if year % 400 == 0:
-                print("It is a leap year")
+    # process and output
+    try:
+        user_input_number = int(user_input_string)
+        if user_input_number % 4 == 0:
+            if user_input_number % 100 == 0:
+                if user_input_number % 400 == 0:
+                    print("It is a leap year".format(user_input_string))
+                else:
+                    print("It is not a leap year".format(user_input_string))
             else:
-                print("It is not a leap year")
+                print("It is a leap year.".format(user_input_string))
+                print("")
         else:
-            print("it is a leap year")
-    else:
-        print("It is not a leap year")
+            print("It is a common year.".format(user_input_string))
+            print("")
+
+    except Exception:
+        print("Really? (-_-)ゞ゛pick a year pls.".format(user_input_string))
     print("\nDone.")
 
 
